236fd17f4320aa1eecc745d51a272fbdd56e4266
Alejandro Ariel Uñate <alejandroarielunate@gmail.com>
2024-06-22 22:15:30 -0300

    agrega mi primer .py

---
+++ src/suma.py
@@ -0,0 +1,16 @@
+a = 3
+b = 4
+a += 1
+c = a + b
+print(c)
+nombre = "ariel"
+apellido = "uñate"
+mensaje = """ todo bien
+y todo mal.
+que se le va a hacer"""
+print(nombre + apellido)
+d = 3.14
+print(a+d)
+# del a
+print(f"nombre: {nombre} Apellido: {apellido} edad: {a+b}")
+print ("ariel" in nombre)
